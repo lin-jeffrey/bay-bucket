51fbfbd7e79263bb7a07710be79be6f0d15e1a2a
Brianna McGovern <bmcgovern007@gmail.com>
2021-11-24 02:36:25 -0800

    add destination fragment

--- app/src/main/java/com/example/baybucket/DestinationActivity.java
+++ app/src/main/java/com/example/baybucket/DestinationActivity.java
@@ -1,14 +1,35 @@
 package com.example.baybucket;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DestinationActivity extends AppCompatActivity {
+
+    Button btnArrived;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination);
+
+        btnArrived = findViewById(R.id.btn_arrived);
+        btnArrived.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showDialog();
+            }
+        });
+    }
+
+    void showDialog() {
+        // Create the fragment and show it as a dialog.
+        DialogFragment newFragment = DestinationFragment.newInstance("CANCEL", "SUBMIT");
+        newFragment.setShowsDialog(true);
+        newFragment.show(getSupportFragmentManager(), "dialog");
     }
 }
