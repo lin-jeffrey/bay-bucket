f316d79c80987323b297842b4870519242ac30ec
abhi-srivathsa <abhilashsrivatsa4656@gmail.com>
2021-11-28 09:44:38 -0800

    renamed leaderboard layout

--- app/src/main/java/com/example/baybucket/LeaderBoard.java
+++ app/src/main/java/com/example/baybucket/LeaderBoard.java
@@ -9,6 +9,6 @@ public class LeaderBoard extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_leader_board);
+        setContentView(R.layout.fragment_leaderboard);
     }
 }
