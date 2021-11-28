1d36be6b6c81ebfbdcb4943d8cd7c5786cbba471
abhi-srivathsa <abhilashsrivatsa4656@gmail.com>
2021-11-28 12:43:58 -0800

    added home functionality

--- app/src/main/java/com/example/baybucket/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/baybucket/ui/home/HomeFragment.java
@@ -23,8 +23,7 @@ public class HomeFragment extends Fragment {
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
 
-    //Tanvi
-    ImageButton sf;
+    ImageButton sf, sc, scr, sj, b, pa;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -34,17 +33,65 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        //Tanvi
-        sf = (ImageButton)root.findViewById(R.id.sanFrancisco);
 
-        //Tanvi
+        sf = (ImageButton)root.findViewById(R.id.sanFrancisco);
+        sc = (ImageButton)root.findViewById(R.id.santaClara);
+        sj = (ImageButton)root.findViewById(R.id.sanJose);
+        scr = (ImageButton)root.findViewById(R.id.santaCruz);
+        b = (ImageButton)root.findViewById(R.id.berkeley);
+        pa = (ImageButton)root.findViewById(R.id.sanFrancisco);
+
+
         sf.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getContext(), BucketList.class);
+                intent.putExtra("name", "san%20francisco");
                 startActivity(intent);
             }
         });
+        sc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), BucketList.class);
+                intent.putExtra("name", "santa%20clara");
+                startActivity(intent);
+            }
+        });
+        sj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), BucketList.class);
+                intent.putExtra("name", "san%20jose");
+                startActivity(intent);
+            }
+        });
+        scr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), BucketList.class);
+                intent.putExtra("name", "santa%20cruz");
+                startActivity(intent);
+            }
+        });
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), BucketList.class);
+                intent.putExtra("name", "berkeley");
+                startActivity(intent);
+            }
+        });
+        pa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getContext(), BucketList.class);
+                intent.putExtra("name", "palo%20alto");
+                startActivity(intent);
+            }
+        });
+
+
 
 //        final TextView textView = binding.textHome;
 //        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
