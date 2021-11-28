3b0da68ab4c5a9f7824e5af40ea2823940b65c87
Brianna McGovern <bmcgovern007@gmail.com>
2021-11-28 01:58:21 -0800

    map on destination activity

--- app/src/main/java/com/example/baybucket/DestinationActivity.java
+++ app/src/main/java/com/example/baybucket/DestinationActivity.java
@@ -1,29 +1,43 @@
 package com.example.baybucket;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-public class DestinationActivity extends AppCompatActivity {
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class DestinationActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     Button btnArrived;
+    MapView mapView;
+
+    private GoogleMap mMap;
+
+    private static final String TAG = "DestinationActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination);
 
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.getMapAsync(this);
+
         btnArrived = findViewById(R.id.btn_arrived);
-        btnArrived.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showDialog();
-            }
-        });
+        btnArrived.setOnClickListener(view -> showDialog());
     }
 
     void showDialog() {
@@ -31,5 +45,15 @@ public class DestinationActivity extends AppCompatActivity {
         DialogFragment newFragment = DestinationFragment.newInstance("CANCEL", "SUBMIT");
         newFragment.setShowsDialog(true);
         newFragment.show(getSupportFragmentManager(), "dialog");
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        Log.i(TAG, "Map ready");
+        mMap = googleMap;
+        // Add a marker on destination and move the camera
+        LatLng scu = new LatLng(37.35, -121.94); // TODO: change to destination coordinates
+        mMap.addMarker(new MarkerOptions().position(scu).title("Marker in SCU"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(scu,15));
     }
 }
