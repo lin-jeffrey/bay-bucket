1e4dcebb072efefc8b7bcb92e9d66e6e3f751f4a
abhi-srivathsa <abhilashsrivatsa4656@gmail.com>
2021-11-29 08:46:55 -0800

    added splash screen and animation

--- app/src/main/java/com/example/baybucket/MainActivity.java
+++ app/src/main/java/com/example/baybucket/MainActivity.java
@@ -4,29 +4,63 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN = 5000;
+
+    Animation topAnim, bottomAnim;
+    ImageView image;
+    TextView title;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim = AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+        image = findViewById(R.id.logo);
+        title = findViewById(R.id.title);
+
+        image.setAnimation(topAnim);
+        title.setAnimation(bottomAnim);
+
 
         FirebaseAuth auth = FirebaseAuth.getInstance();
 
         if (auth.getCurrentUser() != null) {
             // User is signed in (getCurrentUser() will be null if not signed in)
-            Intent intent = new Intent(this, Home.class);
-            startActivity(intent);
-            finish();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this, Home.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },SPLASH_SCREEN);
         }
         else{
-            Intent intent = new Intent(this, Login.class);
-            startActivity(intent);
-            finish();
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this, Login.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },SPLASH_SCREEN);
         }
 
     }
